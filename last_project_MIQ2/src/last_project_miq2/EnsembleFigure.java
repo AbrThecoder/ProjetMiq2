bc679fb047590dbd83197049c047ea8cc025564c
AbrThecoder <45606320+AbrThecoder@users.noreply.github.com>
2019-01-06 17:31:08 +0100

    Add files via upload

---
+++ last_project_MIQ2/src/last_project_miq2/EnsembleFigure.java
@@ -0,0 +1,166 @@
+package last_project_miq2;
+
+import last_project_miq2.Point;
+import last_project_miq2.Figure;
+import javax.swing.*;
+import java.util.ArrayList;
+
+import static last_project_miq2.util.max;
+import static last_project_miq2.util.min;
+
+public class EnsembleFigure extends Figure {
+
+    //attributs
+    private static int compteurId = 0;
+    //utils
+    private ArrayList<Figure> figures = new ArrayList<>();
+
+    //Constructeurs
+
+    public EnsembleFigure() {
+        super.setIdFig("EnsembleFigure_N°" + compteurId);
+        compteurId++;
+    }
+
+    public EnsembleFigure(ArrayList<Figure> figures) {
+        this.figures = figures;
+        super.setIdFig("EnsembleFigure_N°" + compteurId);
+        compteurId++;
+    }
+
+    public EnsembleFigure(ArrayList<Figure> figures, String id) {
+        this.figures = figures;
+        super.setIdFig("EnsembleFigure_N°" + compteurId);
+        super.setIdFig(id);
+    }
+
+    //methodes
+
+    public void afficheFigure() {
+        /*
+        for (int i= 0 ; i<figures.size() ; i++){
+            System.out.println("index" + i + "\t" + figures.get(i));
+        */
+        System.out.println(this);
+    }
+
+    public ArrayList<Figure> getAll() { //retourne toutes les figures
+        return figures;
+    }
+
+    public void setAll(ArrayList<Figure> figures) { //change toute la liste de figure par un autre entrée
+        this.figures = figures;
+    }
+
+    public Figure get(int index) { //retourne la figure a un indice donné
+        return figures.get(index);
+    }
+
+    public void remove(int index) {// retire la figure a aun indice donné
+        figures.remove(index);
+    }
+
+    public void clear() { //retire toutes les figures
+        figures.clear();
+    }
+
+    public void add(Figure addedFig) { //ajoute une figure a l'ensemble
+        figures.add(addedFig);
+    }
+
+    public int size() { //retourne le nombre de figure presente dans la liste
+        return figures.size();
+    }
+
+
+    @Override
+    public void paint(JPanel jPanel){
+        for (int  i = 0; i<this.figures.size();i++){
+            this.figures.get(i).paint(jPanel);
+        }
+    }
+    public void depaint(JPanel jPanel){
+        this.figures.clear();
+        for (int  i = 0; i<this.figures.size();i++){
+            this.figures.get(i).paint(jPanel);
+        }
+        jPanel.repaint();
+    }
+    
+
+    @Override
+    public double distancePoint(Point A) {
+        ArrayList<Double> dist = new ArrayList<Double>();
+        for (int i = 0; i < figures.size(); i++) {
+            dist.add(figures.get(i).distancePoint(A));
+        }
+        return min(dist);
+    }
+
+    @Override
+    public double maxX() {
+        ArrayList<Double> maxsX = new ArrayList<Double>();
+        for (int i = 0; i < figures.size(); i++) {
+            if (!(figures.get(i) instanceof EnsembleFigure))
+                maxsX.add(figures.get(i).maxX());
+            else {
+                for (int j = 0; j < ((EnsembleFigure) figures.get(i)).size(); i++) {
+                    maxsX.add(figures.get(i).maxX());
+                }
+            }
+
+        }
+        return max(maxsX);
+    }
+
+    @Override
+    public double minX() {
+        ArrayList<Double> minsX = new ArrayList<Double>();
+        for (int i = 0; i < figures.size(); i++) {
+            minsX.add(figures.get(i).minX());
+        }
+        return min(minsX);
+    }
+
+    @Override
+    public double maxY() {
+        ArrayList<Double> maxsY = new ArrayList<Double>();
+        for (int i = 0; i < figures.size(); i++) {
+            maxsY.add(figures.get(i).maxY());
+        }
+        return max(maxsY);
+    }
+
+    @Override
+    public double minY() {
+        ArrayList<Double> minsY = new ArrayList<Double>();
+        for (int i = 0; i < figures.size(); i++) {
+            minsY.add(figures.get(i).minY());
+        }
+        return min(minsY);
+    }
+
+    @Override
+    public String toTxt() { //methode permettant d'ecrire dans le "language" de l'algorithme utilisé pour lire les fichiers avec le programme
+        String str = "<EnsembleFigure>\n";
+        for (int i = 0; i < this.size(); i++) {
+            str += this.get(i).toTxt();
+        }
+        str += "</EnsembleFigure>\n" + this.getIdFig() + "\n";
+        return str;
+    }
+
+    @Override
+    public String toString() {
+        String str = "EnsembleFigure[";
+        if (this.size() > 0) {
+            for (int i = 0; i < this.size(); i++) {
+                str += this.get(i).toString() + ", ";
+            }
+            str = str.substring(0, str.length() - 2);
+            str += ", ";
+        }
+        str += "idFig='" + super.getIdFig() + "']";
+        return str;
+    }
+}
---
+++ last_project_MIQ2/src/last_project_miq2/Segment.java
@@ -0,0 +1,130 @@
+package last_project_miq2;
+
+import last_project_miq2.Point;
+import last_project_miq2.Figure;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static last_project_miq2.util.max;
+import static last_project_miq2.util.min;
+
+
+public class Segment extends Figure {
+    //statics
+    private static int compteurId = 0;
+    //attributs
+    private Point P0;
+    private Point P1;
+
+    //constructeurs
+
+    public Segment(Point p0, Point p1, String id) {
+        this.P0 = p0;
+        this.P1 = p1;
+        super.setIdFig(id);
+    }
+
+    public Segment(Point p0, Point p1) {
+        this.P0 = p0;
+        this.P1 = p1;
+        compteurId++;
+        super.setIdFig("Segment_N°" + compteurId);
+    }
+
+    public Segment() {
+        System.out.println("Pour créer le segment nous allons créer 2 points");
+        System.out.println("Point 1 :");
+        this.P0 = new Point();
+        System.out.println("Point 2 :");
+        this.P1 = new Point();
+        super.setIdFig("Segment_N°" + compteurId);
+        compteurId++;
+    }
+
+
+    //methodes
+
+    public double longueurSegment() {
+        return this.P0.distancePoint(P1);
+    }
+
+    public double distancePoint(Point p3) {
+        //cf documentation pdf
+        double up = (p3.getX() - this.P0.getX()) * (this.P1.getX() - this.P0.getX()) + (p3.getY() - this.P0.getY()) * (this.P1.getX() - this.P0.getY());
+        up /= Math.pow((this.P1.getX() - this.P0.getX()), 2) + Math.pow((this.P1.getY() - this.P0.getY()), 2);
+
+        //3 cas differents selon up cf doc
+        if (up < 0)
+            return this.P0.distancePoint(p3);
+        if (up > 1)
+            return this.P1.distancePoint(p3);
+        else {
+            Point p4 = new Point(this.P0.getX() + up * (this.P1.getX() - this.P0.getX()),
+                    this.P0.getY() + up * (this.P1.getY() - this.P0.getY()));
+            return p3.distancePoint(p4);
+        }
+    }
+
+    public Point getP0() {
+        return P0;
+    }
+
+    public void setP0(Point P0) {
+        this.P0 = P0;
+    }
+
+    public Point getP1() {
+        return P1;
+    }
+
+    public void setP1(Point P1) {
+        this.P1 = P1;
+    }
+
+    @Override
+    public void paint(JPanel jPanel){
+        Graphics2D graphics2D = (Graphics2D)jPanel.getGraphics();
+        graphics2D.drawLine((int)P0.getX(),(int)P0.getY(),(int)P1.getX(),(int)P1.getY()); 
+    }
+    public void depaint(JPanel jPanel){
+        Graphics2D graphics2D = (Graphics2D)jPanel.getGraphics();
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.drawLine((int)P0.getX(),(int)P0.getY(),(int)P1.getX(),(int)P1.getY()); 
+    }
+
+    @Override
+    public double maxX() {
+        return max(this.P0.getX(), this.P1.getX());
+    }
+
+    @Override
+    public double minX() {
+        return min(this.P0.getX(), this.P1.getX());
+    }
+
+    @Override
+    public double maxY() {
+        return max(this.P0.getY(), this.P1.getY());
+    }
+
+    @Override
+    public double minY() {
+        return min(this.P0.getY(), this.P1.getY());
+    }
+
+    @Override
+    public String toTxt() {//methode permettant d'ecrire dans le "language" de l'algorithme utilisé pour lire les fichiers avec le programme
+        return "<Segment>\n" + this.P0.toTxt() + this.P1.toTxt() + this.getIdFig() + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return "Segment{" +
+                "P0=" + P0 +
+                ", P1=" + P1 +
+                ", idFig='" + super.getIdFig() + '\'' +
+                '}';
+    }
+
+}
